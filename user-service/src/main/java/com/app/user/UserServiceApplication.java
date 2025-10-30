cd02c912653eb88efd24b5b4320c7a9ed8d0da16
Katya1803 <vuvuworkplace@gmail.com>
2025-10-30 15:52:06 +0700

    Refactor system

--- user-service/src/main/java/com/app/user/UserServiceApplication.java
+++ user-service/src/main/java/com/app/user/UserServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.app.user",
@@ -11,6 +14,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 })
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.app.user.repository")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
