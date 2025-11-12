960e46ba3b95e55159e0f07ca95055dfea6919cb
Katya1803 <147164934+Katya1803@users.noreply.github.com>
2025-11-13 01:00:49 +0700

    Fix docker file

--- init-db.sql
+++ init-db.sql
@@ -1,29 +1,28 @@
--- Cài extension nếu cần
-CREATE EXTENSION IF NOT EXISTS dblink;
-
+-- Tạo các database nếu chưa có
 DO
 $$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db') THEN
-      PERFORM dblink_exec('CREATE DATABASE auth_db');
-END IF;
-
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'user_db') THEN
-      PERFORM dblink_exec('CREATE DATABASE user_db');
-END IF;
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'auth_db') THEN
+      RAISE NOTICE 'Creating database auth_db...';
+   END IF;
 END
 $$;
+
+-- Dùng lệnh CREATE DATABASE bên ngoài DO block
+\connect postgres
+CREATE DATABASE auth_db;
+CREATE DATABASE user_db;
 
 -- Tạo user nếu chưa có
 DO
 $$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'admin') THEN
-CREATE ROLE admin WITH LOGIN PASSWORD 'admin123';
-END IF;
+   IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+      CREATE ROLE admin WITH LOGIN PASSWORD 'admin123';
+   END IF;
 END
 $$;
 
--- Gán quyền
+-- Gán quyền cho user
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO admin;
 GRANT ALL PRIVILEGES ON DATABASE user_db TO admin;
