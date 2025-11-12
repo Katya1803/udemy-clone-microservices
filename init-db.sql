65c73b384bf5fa39b16df5311ca3bb28d5f1f48a
Katya1803 <vuvuworkplace@gmail.com>
2025-11-12 15:41:45 +0700

    Implement ELK stack with docker

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,29 @@
-CREATE DATABASE IF NOT EXISTS auth_db;
-CREATE DATABASE IF NOT EXISTS user_db;
+-- Cài extension nếu cần
+CREATE EXTENSION IF NOT EXISTS dblink;
 
--- Grant permissions
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db') THEN
+      PERFORM dblink_exec('CREATE DATABASE auth_db');
+END IF;
+
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'user_db') THEN
+      PERFORM dblink_exec('CREATE DATABASE user_db');
+END IF;
+END
+$$;
+
+-- Tạo user nếu chưa có
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'admin') THEN
+CREATE ROLE admin WITH LOGIN PASSWORD 'admin123';
+END IF;
+END
+$$;
+
+-- Gán quyền
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO admin;
 GRANT ALL PRIVILEGES ON DATABASE user_db TO admin;
